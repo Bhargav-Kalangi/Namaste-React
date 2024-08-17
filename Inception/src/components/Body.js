ed4c069fafa596cf152d8eb15917d80649237357
Bhargav-Kalangi <ambhargav2021@gmail.com>
2024-08-18 00:19:53 +0530

    Episode-6 exploring the world

--- Inception/src/components/Body.js
+++ Inception/src/components/Body.js
@@ -1,15 +1,28 @@
-import resList from "../utils/resList";
+// import resList from "../utils/resList";
 import RestoCards from "./Restocards";
-import { useState } from "react";
+
+import { useState,useEffect } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-  const [listOfRestCards,setListOfRestCards]=useState(resList);
-  console.log(listOfRestCards)
+  const [listOfRestCards,setListOfRestCards]=useState([]);
+  // console.log(listOfRestCards)
+ useEffect(()=>{
+APIDATA();
+ })
+ const APIDATA=async()=>{
+  let data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+  let json=await data.json();
+   
+   setListOfRestCards(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+ }
+
  
-    return (
+    return (listOfRestCards.length===0)?<Shimmer/>: (
       <div className="body">
         <button className="filter-btn" onClick={()=>{
-          const filteredData=listOfRestCards.filter((res) =>res.info.avgRating >4.2)
+          const filteredData=listOfRestCards.filter((res) =>res.info.avgRating >4.1)
           setListOfRestCards(filteredData)
+        
 
         }}>Top Rated Restaurants</button>
         <div className="resto-cards">
--- Inception/src/components/Header.js
+++ Inception/src/components/Header.js
@@ -1,6 +1,7 @@
 import { LOGO_URL } from "../utils/constants";
-
+import { useState } from "react";
 const Header = () => {
+  const[btnName,setBtnName]=useState('Login')
     return (
       <div className="header">
         <div className="logo-container">
@@ -15,6 +16,9 @@ const Header = () => {
             <li>About Us</li>
             <li>Contact us</li>
             <li>Add to cart</li>
+            <button className="login-btn" onClick={()=>{
+              btnName==='Login'?setBtnName('Logout'):setBtnName('Login')
+            }}>{btnName}</button>
           </ul>
         </div>
       </div>
